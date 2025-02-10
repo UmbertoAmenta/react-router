941c87a2a2e455d56cde6529b3b57b93cf021c1b
Umberto Amenta <umberto.amenta@hotmail.it>
2025-02-10 18:48:35 +0100

    feat: +about-ut page

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,13 +1,57 @@
 export default function AboutUs() {
   return (
     <>
-      <h1>Chi siamo?</h1>
-      <p>
-        Un associazione di Lorem ipsum, dolor sit amet consectetur adipisicing
-        elit. Quae ipsam odit alias blanditiis. Harum dignissimos debitis
-        commodi est temporibus necessitatibus obcaecati, ratione qui! Voluptatem
-        debitis magni praesentium esse a impedit.
-      </p>
+      <div className=" contact-us container ">
+        <h2>Chi siamo</h2>
+        <p>
+          Benvenuti nel nostro angolo di paradiso culinario e creativo! Siamo un
+          gruppo di nonne con una passione sfrenata per la cucina, l'uncinetto e
+          lo sviluppo web. Ogni giorno è un'opportunità per unire le nostre
+          ricette tradizionali con un tocco di modernità, mentre intrecciamo
+          fili di lana con linee di codice.
+        </p>
+
+        <div>
+          <h3>La nostra cucina</h3>
+          <p>
+            Le nostre cucine sono il cuore pulsante delle nostre case, dove
+            mescoliamo tradizione e innovazione. Ogni ricetta che condividiamo è
+            un pezzo della nostra storia, arricchita da anni di esperienza e
+            amore per il cibo. Dalla pasta fatta in casa ai dolci da forno,
+            vogliamo portare un pezzo delle nostre radici direttamente sulle
+            vostre tavole.
+          </p>
+        </div>
+
+        <div>
+          <h3>Lavori all'uncinetto</h3>
+          <p>
+            Quando non siamo occupate a cucinare, ci trovate spesso con un
+            uncinetto in mano, creando meraviglie di lana e filo. Ogni punto è
+            lavorato con cura e dedizione, trasformando semplici materiali in
+            opere d'arte uniche. Condividiamo i nostri progetti e tutorial per
+            ispirarvi a esplorare la bellezza dell'arte dell'uncinetto.
+          </p>
+        </div>
+
+        <div>
+          <h3>Sviluppo web</h3>
+          <p>
+            La nostra passione non si ferma alla cucina e all'uncinetto. Siamo
+            anche appassionate di tecnologia e sviluppo web. Grazie a questa
+            passione, abbiamo creato questo blog per condividere con voi le
+            nostre avventure culinarie e creative. Crediamo che non sia mai
+            troppo tardi per imparare qualcosa di nuovo, e ci divertiamo a
+            fondere la nostra conoscenza tradizionale con le competenze
+            digitali.
+          </p>
+        </div>
+
+        <p>
+          Speriamo che il nostro blog vi ispiri a cucinare, creare e imparare
+          insieme a noi. Grazie per essere parte della nostra community!
+        </p>
+      </div>
     </>
   );
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,11 @@
 export default function HomePage() {
-  return <h1>Sono la HomePAGE</h1>;
+  return (
+    <>
+      <h1>Sono la HomePAGE</h1>
+      <p>
+        work in progress, abbiate pazienza, lavoreremo a breve sul database per
+        le nostre, ma anche vostre, ricette.
+      </p>
+    </>
+  );
 }
